ff30758b7300d2e21731ef41434a39e97db5a9af
Sandeep Kumar Jena <sandeepkumar637053@gmail.com>
2024-04-04 19:56:01 +0530

    remove proxy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api/v1/user': 'https://mern-job-seeking-ngkz.onrender.com',
-      '/api/v1/application': 'https://mern-job-seeking-ngkz.onrender.com',
-      '/api/v1/job': 'https://mern-job-seeking-ngkz.onrender.com',
-    }
-  }
 })
