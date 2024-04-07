68c82d873701fb8ff7218cd91af2b3f12459acfe
Sandeep Kumar Jena <sandeepkumar637053@gmail.com>
2024-04-07 19:15:36 +0530

    update user controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -99,7 +99,8 @@ export const login = asyncHandler(async (req, res) => {
 export const logout = asyncHandler(async (req,res) => {
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: "none"
     }
 
     return res.status(200)
